0ac99009b836765ef5ae55d89d20266a2932dcef
Lorenzo Lotti <52128675+LorenzoLotti@users.noreply.github.com>
2021-02-09 20:12:55 +0100

    Add files via upload

--- js/preload.js
+++ js/preload.js
@@ -4,8 +4,8 @@ var res;
 
 // Load the background animation.
 res = new Image();
-res.src = '../imgs/animation.gif';
 res.onload = function()
 {
   document.getElementById('page').classList.add('loaded');
 };
+res.src = '../imgs/animation.gif';
